2648691dcd3e629ebcdac32fa0e3a95f7f88e543
Rec0iL99 <joelmathewkoshy@karunya.edu.in>
2020-04-23 13:17:19 +0530

    Project init

---
+++ app/src/main/java/com/khacks/kapp_frontend/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.khacks.kapp_frontend
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
