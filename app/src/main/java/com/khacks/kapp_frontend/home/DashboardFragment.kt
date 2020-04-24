385bd52e41dc188d2ee4bbf7aa5b8b737e996d2a
Rec0iL99 <joelmathewkoshy@karunya.edu.in>
2020-04-25 00:45:51 +0530

    Small code refractoring

--- app/src/main/java/com/khacks/kapp_frontend/home/DashboardFragment.kt
+++ app/src/main/java/com/khacks/kapp_frontend/home/DashboardFragment.kt
@@ -1,20 +1,19 @@
-package com.khacks.kapp_frontend
+package com.khacks.kapp_frontend.home
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.khacks.kapp_frontend.R.layout
 
-class StarredFragment : Fragment() {
-
+class DashboardFragment : Fragment() {
   override fun onCreateView(
     inflater: LayoutInflater,
     container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View? {
-    var view: View = inflater.inflate(R.layout.fragment_starred, container, false)
+    var view: View = inflater.inflate(layout.fragment_dashboard, container, false)
     return view
   }
-
 }
--- app/src/main/java/com/khacks/kapp_frontend/home/StarredFragment.kt
+++ app/src/main/java/com/khacks/kapp_frontend/home/StarredFragment.kt
@@ -1,18 +1,21 @@
-package com.khacks.kapp_frontend
+package com.khacks.kapp_frontend.home
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.khacks.kapp_frontend.R.layout
 
-class DashboardFragment : Fragment() {
+class StarredFragment : Fragment() {
+
   override fun onCreateView(
     inflater: LayoutInflater,
     container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View? {
-    var view: View = inflater.inflate(R.layout.fragment_dashboard, container, false)
+    var view: View = inflater.inflate(layout.fragment_starred, container, false)
     return view
   }
+
 }
