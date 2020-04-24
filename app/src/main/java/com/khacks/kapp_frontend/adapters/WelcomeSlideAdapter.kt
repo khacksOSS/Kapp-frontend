422d46136c7fcdf3e4d2c6c3390e1ccd99aba645
Rec0iL99 <joelmathewkoshy@karunya.edu.in>
2020-04-24 14:28:04 +0530

    Made all files follow SquareAndroid codestyle

--- app/src/main/java/com/khacks/kapp_frontend/adapters/WelcomeSlideAdapter.kt
+++ app/src/main/java/com/khacks/kapp_frontend/adapters/WelcomeSlideAdapter.kt
@@ -10,36 +10,36 @@ import com.khacks.kapp_frontend.R
 import com.khacks.kapp_frontend.welcome.WelcomeSlide
 
 class WelcomeSlideAdapter(private val welcomeSlides: List<WelcomeSlide>)
-    : RecyclerView.Adapter<WelcomeSlideAdapter.IntroSlideViewHolder>() {
+  : RecyclerView.Adapter<WelcomeSlideAdapter.IntroSlideViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IntroSlideViewHolder {
-        return IntroSlideViewHolder(
-            LayoutInflater.from(parent.context).inflate(
-                R.layout.slider_item_container,
-                parent,
-                false
-            )
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IntroSlideViewHolder {
+    return IntroSlideViewHolder(
+        LayoutInflater.from(parent.context).inflate(
+            R.layout.slider_item_container,
+            parent,
+            false
         )
-    }
+    )
+  }
 
-    override fun getItemCount(): Int {
-        return welcomeSlides.size
-    }
+  override fun getItemCount(): Int {
+    return welcomeSlides.size
+  }
 
-    override fun onBindViewHolder(holder: IntroSlideViewHolder, position: Int) {
-        holder.bind(welcomeSlides[position])
-    }
+  override fun onBindViewHolder(holder: IntroSlideViewHolder, position: Int) {
+    holder.bind(welcomeSlides[position])
+  }
 
-    inner class IntroSlideViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        private val titleText = view.findViewById<TextView>(R.id.intro_title)
-        private val descText = view.findViewById<TextView>(R.id.intro_desc)
-        private val illustration = view.findViewById<ImageView>(R.id.intro_image)
+  inner class IntroSlideViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    private val titleText = view.findViewById<TextView>(R.id.intro_title)
+    private val descText = view.findViewById<TextView>(R.id.intro_desc)
+    private val illustration = view.findViewById<ImageView>(R.id.intro_image)
 
-        fun bind(welcomeSlide: WelcomeSlide) {
-            titleText.text = welcomeSlide.title
-            descText.text = welcomeSlide.desc
-            illustration.setImageResource(welcomeSlide.icon)
-        }
+    fun bind(welcomeSlide: WelcomeSlide) {
+      titleText.text = welcomeSlide.title
+      descText.text = welcomeSlide.desc
+      illustration.setImageResource(welcomeSlide.icon)
     }
+  }
 
 }
--- app/src/main/java/com/khacks/kapp_frontend/welcome/WelcomeSlide.kt
+++ app/src/main/java/com/khacks/kapp_frontend/welcome/WelcomeSlide.kt
@@ -1,7 +1,7 @@
 package com.khacks.kapp_frontend.welcome
 
 data class WelcomeSlide (
-    val title: String,
-    val desc: String,
-    val icon: Int
+  val title: String,
+  val desc: String,
+  val icon: Int
 )
