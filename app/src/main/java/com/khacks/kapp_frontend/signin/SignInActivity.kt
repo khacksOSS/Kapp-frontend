e682e3fa6d5c672ea5add94725fcca12701b140e
Rec0iL99 <joelmathewkoshy@karunya.edu.in>
2020-05-13 05:02:34 +0530

    Designed sign in activity

--- app/src/main/java/com/khacks/kapp_frontend/signin/SignInActivity.kt
+++ app/src/main/java/com/khacks/kapp_frontend/signin/SignInActivity.kt
@@ -13,21 +13,12 @@ class SignInActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_signin)
-    customActionBar()
 
-    bt_sigin.setOnClickListener {
+    bt_signin.setOnClickListener {
       var intent: Intent = Intent(this@SignInActivity, MainActivity::class.java)
       intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
       startActivity(intent)
     }
   }
 
-  //function to modify actionbar
-  private fun customActionBar() {
-    val actionBar: ActionBar? = supportActionBar
-    actionBar?.title = "Sign In"
-    actionBar?.elevation = 0F
-    actionBar?.setHomeAsUpIndicator(R.drawable.back_button)
-    actionBar?.setDisplayHomeAsUpEnabled(true)
-  }
 }
