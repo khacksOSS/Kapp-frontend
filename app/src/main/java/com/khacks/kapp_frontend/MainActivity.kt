115565f7d5d53486e3895105bdec9740c4290e38
Rec0iL99 <joelmathewkoshy@karunya.edu.in>
2020-05-13 14:01:21 +0530

    designed main screen

--- app/src/main/java/com/khacks/kapp_frontend/MainActivity.kt
+++ app/src/main/java/com/khacks/kapp_frontend/MainActivity.kt
@@ -6,8 +6,8 @@ import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_main.action_bar_title
 import kotlinx.android.synthetic.main.activity_main.bottom_navigation
-import kotlinx.android.synthetic.main.custom_action_bar.view.action_bar_title
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,19 +16,11 @@ class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
-    customActionBar()
 
     //bottom navigation controller
     navController = Navigation.findNavController(this, R.id.nav_host_fragment)
     NavigationUI.setupWithNavController(bottom_navigation, navController)
-  }
 
-  //function to modify actionbar
-  private fun customActionBar() {
-    var toolbar: Toolbar = findViewById(R.id.main_custom_action_bar) as Toolbar
-    setSupportActionBar(toolbar)
-    actionBar?.setDisplayHomeAsUpEnabled(true)
-    //here the username will replace 'Joel'
-    toolbar.action_bar_title.text = "Welcome, Joel"
+    action_bar_title.text = "Welcome, Joel"
   }
 }
